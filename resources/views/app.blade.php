d921b1f1aa1fabad84aafb77e3135e50a8d6ca89
mahadi hassan <mahadi@aapbd.com>
2023-11-28 21:40:40 +0600

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>IT-Firm IT Businees HTML-5 Template | Homepage 03</title>
+    <!-- Stylesheets -->
+    <Link href="{{asset('Frontend/css/bootstrap.css')}}" rel="stylesheet">
+    <Link href="{{asset('Frontend/css/style.css')}}" rel="stylesheet">
+    <Link href="{{asset('Frontend/css/responsive.css')}}" rel="stylesheet">
+
+    <Link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <Link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <Link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <Link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <Link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+    <!-- Responsive -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    @vite('resources/js/app.js')
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+  <script src="{{asset('Frontend/js/jquery.js')}}"></script>
+  <script src="{{asset('Frontend/js/appear.js')}}"></script>
+  <script src="{{asset('Frontend/js/owl.js')}}"></script>
+  <script src="{{asset('Frontend/js/wow.js')}}"></script>
+  <script src="{{asset('Frontend/js/odometer.js')}}"></script>
+  <script src="{{asset('Frontend/js/nav-tool.js')}}"></script>
+  <script src="{{asset('Frontend/js/mixitup.js')}}"></script>
+  <script src="{{asset('Frontend/js/popper.min.js')}}"></script>
+  <script src="{{asset('Frontend/js/parallax.min.js')}}"></script>
+  <script src="{{asset('Frontend/js/parallax-scroll.js')}}"></script>
+  <script src="{{asset('Frontend/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('Frontend/js/tilt.jquery.min.js')}}"></script>
+  <script src="{{asset('Frontend/js/magnific-popup.min.js')}}"></script>
+  
+  <script src="{{asset('Frontend/js/script.js')}}"></script>
+</html>
